3c4b6658c90198ec169a445248a9fa18801e796e
melakuzeleke <116151311+melbatestgithub@users.noreply.github.com>
2024-02-10 11:04:12 +0300

    Update config.js

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+const development = {
+  apiUrl: "http://localhost:6500/api/v1",
+  mongoDbUri:
+    "mongodb+srv://travel:travel123@cluster0.8ndlf0f.mongodb.net/travelApp",
+};
+
+const production = {
+  apiUrl: process.env.REACT_NATIVE_API_URI,
+  mongoDbUri: process.env.MONGODB_URI,
+};
+
+// Determine environment
+const environment = process.env.NODE_ENV || "development";
+
+// Export configuration based on environment
+module.exports = environment === "production" ? production : development;
